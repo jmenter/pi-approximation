708c802aec5d424159a27e0a50963a24e93d1777
Jeff Menter <jmenter@gmail.com>
2019-02-03 17:50:05 -0600

    refactor

--- code.js
+++ code.js
@@ -7,20 +7,18 @@ var isDrawing = false;
 function draw() {
     totalCount += pointsToAddCount;
     randomPoints(pointsToAddCount).forEach(point => {
-        let distanceFromCenter = Math.sqrt((point.x * point.x) + (point.y * point.y));
-        let isInside = distanceFromCenter < radius;
-        if (isInside) {
+        if (Math.sqrt((point.x * point.x) + (point.y * point.y)) < radius) {
+            context.fillStyle = "#ff0000";
             innerCount++;
+        } else {
+            context.fillStyle = "#000000";
         }
-        context.fillStyle = isInside ? "#ff0000" : "#000000";
         context.fillRect(point.x + radius - 0.5, point.y + radius - 0.5, 1, 1);
     });
 
     document.getElementById("results").innerText =
         " pi approximation: " + (innerCount / totalCount * 4);
-    if (isDrawing) {
-        window.requestAnimationFrame(draw);
-    }
+    isDrawing ? window.requestAnimationFrame(draw) : null;
 }
 
 function randomPoints(count) {
@@ -34,19 +32,17 @@ function randomPoints(count) {
 function goClick() {
     isDrawing = !isDrawing;
     document.getElementById("button").innerText = isDrawing ? "stop" : "begin";
-    if (isDrawing) {
-        draw();
-    }
+    isDrawing ? draw() : null;
 }
 
 function resetCanvas() {
     diameter = parseFloat(document.getElementById("diameter").value);
-    radius = diameter / 2;
     canvas = document.getElementById("sandbox");
     canvas.width = diameter;
     canvas.height = diameter;
-    context = canvas.getContext("2d");
+    radius = diameter / 2;
 
+    context = canvas.getContext("2d");
     context.fillStyle = "#eeeeee";
     context.fillRect(0, 0, diameter, diameter);
     context.fillStyle = "#777777";
