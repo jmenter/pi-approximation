addbf1cdfe7f0058b53604d45bf799ca872803d4
Jeff Menter <jmenter@gmail.com>
2019-03-15 13:32:26 -0500

    scales canvas for retina displays

--- code.js
+++ code.js
@@ -1,13 +1,38 @@
-var diameter, radius, canvas, context;
-let pointsToAddCount = 5000;
+let POINTS_TO_ADD = 5000;
+
+var diameter, radius, canvas, context, scaledDiameter;
 var innerCount = 0;
 var totalCount = 0;
 var isDrawing = false;
 
+function resetCanvas() {
+    var scale = window.devicePixelRatio;
+    diameter = parseFloat(document.getElementById("diameter").value);
+    scaledDiameter = diameter * scale;
+    canvas = document.getElementById("sandbox");
+    canvas.style.width = diameter + "px";
+    canvas.style.height = diameter + "px";
+    canvas.width = scaledDiameter;
+    canvas.height = scaledDiameter;
+    radius = scaledDiameter / 2;
+
+    context = canvas.getContext("2d");
+    context.fillStyle = "#eeeeee";
+    context.fillRect(0, 0, scaledDiameter, scaledDiameter);
+    context.fillStyle = "#777777";
+    context.fillRect(0, radius, scaledDiameter, 1 * scale);
+    context.fillRect(radius, 0, 1 * scale, scaledDiameter);
+}
+
+function goWasClicked() {
+    isDrawing = !isDrawing;
+    document.getElementById("button").innerText = isDrawing ? "stop" : "begin";
+    isDrawing ? draw() : null;
+}
+
 function draw() {
-    totalCount += pointsToAddCount;
-    randomPoints(pointsToAddCount).forEach(point => {
-        if (Math.sqrt((point.x * point.x) + (point.y * point.y)) < radius) {
+    randomPoints(POINTS_TO_ADD).forEach(point => {
+        if (pointIsInRadius(point, radius)) {
             context.fillStyle = "#ff0000";
             innerCount++;
         } else {
@@ -15,37 +40,25 @@ function draw() {
         }
         context.fillRect(point.x + radius - 0.5, point.y + radius - 0.5, 1, 1);
     });
+    totalCount += POINTS_TO_ADD;
 
     document.getElementById("results").innerText =
-        " pi approximation: " + (innerCount / totalCount * 4);
-    isDrawing ? window.requestAnimationFrame(draw) : null;
+        " pi approximation: " + (innerCount / totalCount * 4) + " using " + totalCount + " dots";
+    window.requestAnimationFrame(isDrawing ? draw : null);
 }
 
 function randomPoints(count) {
-    return new Array(count).fill(0).map(() => {
-        return {
-            x: (Math.random() * diameter) - radius, y: (Math.random() * diameter) - radius
-        };
-    });
+    return new Array(count).fill(0).map(() => randomPoint());
 }
 
-function goClick() {
-    isDrawing = !isDrawing;
-    document.getElementById("button").innerText = isDrawing ? "stop" : "begin";
-    isDrawing ? draw() : null;
+function randomPoint() {
+    return {
+        x: (Math.random() * scaledDiameter) - radius,
+        y: (Math.random() * scaledDiameter) - radius
+    }
 }
 
-function resetCanvas() {
-    diameter = parseFloat(document.getElementById("diameter").value);
-    canvas = document.getElementById("sandbox");
-    canvas.width = diameter;
-    canvas.height = diameter;
-    radius = diameter / 2;
-
-    context = canvas.getContext("2d");
-    context.fillStyle = "#eeeeee";
-    context.fillRect(0, 0, diameter, diameter);
-    context.fillStyle = "#777777";
-    context.fillRect(0, radius, diameter, 1);
-    context.fillRect(radius, 0, 1, diameter);
+function pointIsInRadius(point, radius) {
+    return Math.sqrt((point.x * point.x) + (point.y * point.y)) < radius;
 }
+
